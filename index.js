c48b7d5a2effb9f214843b67ddfe496df08887e4
Khang2561 <21522194@gm.uit.edu.vn>
2023-10-29 15:52:10 +0700

    attack box for player and enemy

--- index.js
+++ index.js
@@ -6,28 +6,56 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)//vẽ hình chữ nhật 
 
-const gravity = 0.2
+const gravity = 0.7 
 
 /*Sprite start*/
 class Sprite{
 
     //hàm mặt định
-    constructor({position, velocity}){
+    constructor({position, velocity, color,offset}){
         this.position = position//vị trí
         this.velocity = velocity//vân tốc 
         this.height = 150
+        this.width = 50
         this.lastKey
+        this.color = this.color
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            height: 50,
+            width: 100,
+        }
+        this.isAttacking
     }
 
     //vẽ nhân vật
     draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50,this.height)//width = 50, height = 100
+        //draw player
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width,this.height)//width = 50, height = 100
+        
+        if (this.isAttacking){
+            //draw attack box 
+            c.fillStyle = 'green'
+            c.fillRect(
+                this.attackBox.position.x, 
+                this.attackBox.position.y, 
+                this.attackBox.width, 
+                this.attackBox.height)
+        }
+        //draw attack box 
+       
     }
 
     update(){
+        //duy chuyển attack box theo nhân vật 
         this.draw()
-        
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
+
         this.position.x += this.velocity.x//duy chuyen trai phai
         this.position.y += this.velocity.y//roi xuong cua nhan vat
         //tạo phạm vi dưới cho bảng đồ 
@@ -37,7 +65,13 @@ class Sprite{
         else{
             this.velocity.y += gravity//thêm trọng lượng cho nhân vật 
         }
+    }
 
+    attack() {
+        this.isAttacking = true
+        setTimeout(() =>{
+            this.isAttacking =false
+        },100)
     }
 }
 /*Sprite end*/
@@ -52,9 +86,13 @@ const player =  new Sprite({
     velocity:{
         x: 0,
         y: 0
+    },
+    offset: {
+        x: 0,
+        y: 0,
     }
 })
-
+player.color = 'red'
 
 //tao ra player 1 end
 
@@ -67,8 +105,14 @@ const enemy =  new Sprite({
     velocity:{
         x: 0,
         y: 0
+    },
+    offset: {
+        x: -50,
+        y: 0,
     }
 })
+
+enemy.color = 'blue'
 
 //tao ra player2 end
 
@@ -87,10 +131,24 @@ const keys = {
     },
     s: {
         pressed: false
+    },
+    ArrowLeft:{
+        pressed:false
+    },
+    ArrowRight:{
+        pressed:false
     }
 }
 
-
+// hàm nhận diện va chạm 
+function rectangulareCollirion({rectangule1, rectangule2}){
+    return (
+        rectangule1.attackBox.position.x + rectangule1.attackBox.width >= 
+        rectangule2.position.x && rectangule1.attackBox.position.x <= rectangule2.position.x + rectangule2.width
+        && rectangule1.attackBox.position.y + rectangule1.attackBox.height >= rectangule2.position.y
+        && rectangule1.attackBox.position.y <= rectangule2.position.y + rectangule2.height
+        )
+}
 
 
 /*Animate start*/
@@ -102,22 +160,53 @@ function animate(){
     enemy.update()
 
     player.velocity.x = 0//dung khi khong con an nut duy chuyen trai phai
+    enemy.velocity.x = 0
 
-    //duy chuyển sang trái sang phải 
-    if (keys.a.pressed && lastKey==='a'){
-        player.velocity.x = -1
+    //duy chuyển sang trái sang phải (player movement)
+    if (keys.a.pressed && player.lastKey ==='a'){
+        player.velocity.x = -5
     }
-    else if(keys.d.pressed && lastKey==='d'){
-        player.velocity.x = 1
+    else if(keys.d.pressed && player.lastKey ==='d'){
+        player.velocity.x = 5
     }
+
+    //duy chuyển sang trái sang phải (enemy movement)
+    if (keys.ArrowLeft.pressed && enemy.lastKey ==='ArrowLeft'){
+        enemy.velocity.x = -5
+    }
+    else if(keys.ArrowRight.pressed && enemy.lastKey ==='ArrowRight'){
+        enemy.velocity.x = 5
+    }
+
+    //Nhận diện vùng va chạm player 
+    if (
+        rectangulareCollirion({
+            rectangule1: player,
+            rectangule2: enemy
+        })
+        && player.isAttacking){
+        player.isAttacking = false
+        console.log('player acttacking');
+    }
+    //Nhận diện vùng va chạm enemy 
+    if (
+        rectangulareCollirion({
+            rectangule1: enemy,
+            rectangule2: player
+        })
+        && enemy.isAttacking){
+        enemy.isAttacking = false
+        console.log('enemy acttacking');
+    }
+
+    player.isAttacking = false
+    enemy.isAttacking =  false
 }
 /*Animate end*/
 
 animate()
 
 //add Event start
-
-
 
 //down
 window.addEventListener('keydown',(event)=>{
@@ -135,20 +224,30 @@ window.addEventListener('keydown',(event)=>{
             break
         //up
         case 'w':
-            keys.w.pressed = true
-            player.lastKey = 'w'
-            player.velocity.y = -10
+            //keys.w.pressed = true
+            //player.lastKey = 'w'
+            player.velocity.y = -20
             break
-        //down
-        case 's':
-            keys.s.pressed = true
-            player.lastKey = 's'
+        case ' ':
+            player.isAttacking = true
             break
+        
 
-        //player 1
+        //player 2
         case 'ArrowRight':
             keys.ArrowRight.pressed = true
             enemy.lastKey='ArrowRight'
+            break
+         case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey='ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -20
+            break 
+        case '/':
+            enemy.isAttacking = true
+            break
     }
 })
 
@@ -163,14 +262,20 @@ window.addEventListener('keyup',(event)=>{
         case 'a':
             keys.a.pressed = false
             break
-        //up
-        case 'w':
-            keys.w.pressed = false
+
+    }
+
+    switch (event.key){
+        //right
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
             break
-        //down
-        case 's':
-            keys.s.pressed = false
+        //left
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
             break
+
+     
     }
 })
 
